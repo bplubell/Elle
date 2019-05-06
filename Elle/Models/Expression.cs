53c812a082265752be6e71054fb4d759842a736e
bplubell <bplubell@outlook.com>
2019-05-05 19:36:51 -0700

    Separate view and view model for edit page

---
+++ Elle/Models/Expression.cs
@@ -0,0 +1,9 @@
+﻿namespace Elle.Models
+{
+    public class Expression
+    {
+        public string Name { get; set; } = "";
+        public string Value { get; set; } = "";
+        public object Result { get; set; } = "";
+    }
+}
---
+++ Elle/ViewModels/EditViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Blazor.Extensions.Storage;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Elle.Models;
+
+namespace Elle.ViewModels
+{
+    public class EditViewModel : ComponentBase
+    {
+        [Inject]
+        protected LocalStorage? LocalStorage { get; private set; }
+
+        private readonly string _key = "expressions";
+
+        protected override async Task OnInitAsync()
+        {
+            if (LocalStorage != null)
+            {
+                Expressions = (await LocalStorage.GetItem<Expression[]>(_key)).ToList();
+            }
+        }
+
+        protected List<Expression> Expressions { get; set; } = new List<Expression>();
+
+        protected void AddExpression() => Expressions.Add(new Expression());
+
+        protected void RemoveExpression(int index) => Expressions.RemoveAt(index);
+
+        protected void Clear()
+        {
+            Expressions = new List<Expression>();
+        }
+
+        protected bool ExpressionNameIsValid(string name)
+        {
+            return (!string.IsNullOrWhiteSpace(name)
+                && Expressions.Count(ex => ex.Name == name) == 1);
+        }
+
+        protected async void SaveLocal()
+        {
+            if (LocalStorage != null)
+            {
+                await LocalStorage.SetItem<Expression[]>(_key, Expressions.ToArray());
+            }
+        }
+
+    }
+}
