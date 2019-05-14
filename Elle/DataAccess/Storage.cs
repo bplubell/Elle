377ccc46015b34e6d4819a3051449dc9aee9180a
bplubell <bplubell@outlook.com>
2019-05-13 19:50:51 -0700

    Wrap LocalStorage behind a Storage interface
    This should make using different storage mechanisms (like server side, when using server-side rendering) easier to implement.

---
+++ Elle/DataAccess/Storage.cs
@@ -0,0 +1,31 @@
+﻿using Blazor.Extensions.Storage;
+using Elle.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elle.DataAccess
+{
+    public class Storage : IStorage
+    {
+        private readonly string _storageKey = "calculators";
+        private readonly LocalStorage _localStorage;
+
+        public Storage(LocalStorage localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async Task<IReadOnlyList<Calculator>> LoadCalculatorsAsync()
+        {
+            Calculator[] calculators = await _localStorage.GetItem<Calculator[]>(_storageKey) ?? new Calculator[] { };
+            return calculators.ToList();
+        }
+
+        public async Task SaveCalculatorsAsync(IList<Calculator> calculators)
+        {
+            await _localStorage.SetItem<Calculator[]>(_storageKey, calculators.ToArray());
+        }
+    }
+}
